d1eb56219b399961acb18b2c4de53072753bf8b6
JackieOuzman <jackie.ouzman@csiro.au>
2024-09-06 09:54:58 +0930

    basic version into r

--- 2_APSIM_NextGen_Rotation_import_data_order_it.R
+++ 2_APSIM_NextGen_Rotation_import_data_order_it.R
@@ -86,7 +86,6 @@ APSIM_NextGen <- APSIM_NextGen %>%  filter(Treatment == "Bute_Nil")
 # add some extra clms
 APSIM_NextGen <- APSIM_NextGen %>% mutate(
   Source = "APSIM_NextG",
-  #Treatment = "0N_applied",
   Yield = Yield_kg/1000,
   soil_water_start = soil_water_start,
   soil_water_sowing = soil_water_sowing,
@@ -149,3 +148,103 @@ write.csv(APSIM_NextGen ,
 
 ################################################################################
 
+
+
+# Daily output files -------------------------------------------------------
+
+
+
+APSIM_NextGen_daily <- read_excel("X:/Riskwi$e/Bute/2_Sims_post_Sep2024/2_NextGen/Bute_5_Rotation_N_bank_v3.xlsx", 
+                            sheet = "DailyReport")
+str(APSIM_NextGen_daily)
+
+## Create a new clm called year
+APSIM_NextGen_daily <- APSIM_NextGen_daily %>% 
+  mutate(Year = year(Date), Source = "NextGen",
+         Treatment = SimulationName,
+         InCropFert = FertNApplied,#check this is the correct clm could be FertAmt 
+         
+         soil_water_start =SwStart ,
+         soil_water_sowing = "NA",
+         soil_water_harvest= "NA",
+         
+         soil_NO3_start = NO3Start,
+         soil_NO3_sowing= "NA", 
+         soil_NO3_harvest ="NA" ,
+         Soil_mineral_N_sowing = "Just use NO3",
+         
+         soil_NH4_start ="not_reported",
+         soil_NH4_sowing ="not_reported",
+         soil_NH4_harvest ="not_reported"
+         
+         ) 
+
+
+
+#add in crop and cultivar
+APSIM_NextGen_daily <- APSIM_NextGen_daily %>% 
+  mutate(
+Crop = case_when(
+  Year  == 2022 & CurrentState  == "wheat_1" ~ "Wheat",
+  Year  == 2023 & CurrentState  == "barley_1"~ "Barley"),
+Cultivar = case_when(
+  Year  == 2022 ~ "mace",
+  Year  == 2023 ~ "commander"),
+Biomass = case_when(
+  Year  == 2022 ~ AboveGroundW_WtKgha,
+  Year  == 2023 ~ AboveGroundB_WtKgha)
+
+)
+
+
+
+
+## sum the soil water for all the depths and No 3 for all the depths
+
+APSIM_NextGen_daily <- APSIM_NextGen_daily %>%
+  mutate(
+    NO3 = `NO3.kgha(1)`+`NO3.kgha(2)`+
+      `NO3.kgha(3)`+`NO3.kgha(4)`+
+      `NO3.kgha(5)`+`NO3.kgha(6)`) %>% 
+  mutate(Soil_mineral_N_sowing = NO3)
+     
+APSIM_NextGen_daily <- APSIM_NextGen_daily %>%
+  mutate(
+    Soil_Water = 
+      `Soil.Water.Volumetric(1)`+`Soil.Water.Volumetric(2)`+
+      `Soil.Water.Volumetric(3)`+`Soil.Water.Volumetric(4)`+
+      `Soil.Water.Volumetric(5)`+`Soil.Water.Volumetric(6)` )     
+
+##
+
+#Just ordering it
+APSIM_NextGen_daily  <- APSIM_NextGen_daily %>% 
+  select(Year , 
+         Source,
+         Treatment  ,
+         InCropFert  , # check this is the correct clm could be FertAmt 
+         Crop ,
+         Cultivar  ,
+         
+         soil_water_start,
+         soil_water_sowing,
+         soil_water_harvest,
+         Soil_Water,
+         
+         soil_NO3_start,
+         soil_NO3_sowing, #umm is this NO3 + NH4?
+         soil_NO3_harvest,
+         NO3,
+         
+         soil_NH4_start,
+         soil_NH4_sowing,
+         soil_NH4_harvest,
+         
+         Soil_mineral_N_sowing,
+         
+         Biomass ,
+         #Yield  ,
+         #InCropRain
+  )
+write.csv(APSIM_NextGen_daily ,
+          "X:/Riskwi$e/Bute/2_Sims_post_Sep2024/To_compare_etc/APSIM_NextGen_Daily.csv", row.names = FALSE )
