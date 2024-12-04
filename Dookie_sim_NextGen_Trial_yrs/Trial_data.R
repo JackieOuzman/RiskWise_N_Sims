038efddbc99bb3eca5e04a6d1149a4e97313d2d3
JackieOuzman <jackie.ouzman@csiro.au>
2024-12-04 13:46:57 +1030

    Neater version to get trial data in lon format. Multiple files

--- Dookie_sim_NextGen_Trial_yrs/Trial_data.R
+++ Dookie_sim_NextGen_Trial_yrs/Trial_data.R
@@ -46,8 +46,8 @@ Trial_setup_outputs <- Trial_setup_outputs %>% filter(!is.na(Treatment))
 
 
 
-## Variable 1 ---------------------------------------------------------------------
-##### 1.select the varibale clms ------------------------------------------------
+## variable 1 ---------------------------------------------------------------------
+##### 1.select the variable clms ------------------------------------------------
 Biomass_DF <- Trial_setup_outputs %>% 
   select(Year, Treatment ,`Biomass flowering (t/ha)`,`Biomass harvest (t/ha)`,
          `Biomass harvest date`)  
@@ -63,15 +63,15 @@ names(Biomass_DF)
 Biomass_DF_long <- pivot_longer(
     Biomass_DF, 
     cols = starts_with("Biomass_at"), 
-    names_to = "Timing", 
-    values_to = "Biomass"
+    names_to = "variable", 
+    values_to = "Value"
   )
 Biomass_DF_long
 
 ##### 4.remove the missing data ------------------------------------------------
-Biomass_DF_long <- Biomass_DF_long %>%  filter(!is.na(Biomass))
+Biomass_DF_long <- Biomass_DF_long %>%  filter(!is.na(Value))
 Biomass_DF_long <- Biomass_DF_long %>%  rename (Date = `Biomass harvest date`)
-Biomass <- Biomass_DF_long %>%  select(Treatment, Date, Biomass)
+Biomass <- Biomass_DF_long %>%  select(Treatment, Date, Value)
 
 ##### 5.tidy up df -------------------------------------------------------------
 
@@ -83,8 +83,8 @@ write.csv(Biomass , "X:/Riskwi$e/Dookie/3_Sims_post_Nov2024/Results/Biomass_Tria
 
 
 
-## Variable 2 ---------------------------------------------------------------------
-##### 1.select the varibale clms ------------------------------------------------
+## variable 2 ---------------------------------------------------------------------
+##### 1.select the variable clms ------------------------------------------------
 names(Trial_setup_outputs)
 
 SoilWater_DF <- Trial_setup_outputs %>% 
@@ -103,22 +103,158 @@ names(SoilWater_DF)
 SoilWater_DF_long <- pivot_longer(
   SoilWater_DF, 
   cols = starts_with("Soil_water_at"), 
-  names_to = "Timing", 
-  values_to = "Soil_water"
+  names_to = "variable", 
+  values_to = "Value"
 )
 SoilWater_DF_long
 
 ##### 4.remove the missing data ------------------------------------------------
-Biomass_DF_long <- Biomass_DF_long %>%  filter(!is.na(Biomass))
-Biomass_DF_long <- Biomass_DF_long %>%  rename (Date = `Biomass harvest date`)
-Biomass <- Biomass_DF_long %>%  select(Treatment, Date, Biomass)
+#SoilWater_DF_long <- SoilWater_DF_long %>%  filter(!is.na(Value))
+
+Soil_water <- SoilWater_DF_long %>%  select(Treatment, Date, Value)
 
 ##### 5.tidy up df -------------------------------------------------------------
 
-rm (Biomass_DF_long, Biomass_DF)
+rm (SoilWater_DF_long, SoilWater_DF)
 
 ##### 6.write varibale df -------------------------------------------------------------
-write.csv(Biomass , "X:/Riskwi$e/Dookie/3_Sims_post_Nov2024/Results/Biomass_Trial.csv", row.names = FALSE )
+write.csv(Soil_water , "X:/Riskwi$e/Dookie/3_Sims_post_Nov2024/Results/SoilWater_Trial.csv", row.names = FALSE )
 
 
-Soil_mineral_N_sowing =`Amount of total mineral N at sowing (kg/ha) -Soil`,
+
+## variable 3 ---------------------------------------------------------------------
+##### 1.select the variable clms ------------------------------------------------
+names(Trial_setup_outputs)
+
+# "Amount of NO3 at sowing (kg/ha) - Soil"                                                     
+# "Amount of NH4 at sowing (kg/ha) - Soil"                                                     
+# "Amount of total mineral N at sowing (kg/ha) -Soil"   
+
+SoilNitrogen_DF <- Trial_setup_outputs %>% 
+  select(Year, Treatment ,"Sowing date" , 
+         "Amount of NO3 at sowing (kg/ha) - Soil" ,
+         "Amount of NH4 at sowing (kg/ha) - Soil"   ,
+         "Amount of total mineral N at sowing (kg/ha) -Soil"
+         )  
+
+##### 2. rename the columns
+SoilNitrogen_DF <- SoilNitrogen_DF %>% rename(
+  Soil_NO3_at_sowing = "Amount of NO3 at sowing (kg/ha) - Soil",
+  Soil_NH4_at_sowing = "Amount of NH4 at sowing (kg/ha) - Soil",
+  Soil_TotalN_at_sowing = "Amount of total mineral N at sowing (kg/ha) -Soil",
+  Date = "Sowing date")
+
+
+##### 3.Make the variable clm data long -----------------------------------------
+
+names(SoilNitrogen_DF)
+SoilNitrogen_DF_long <- pivot_longer(
+  SoilNitrogen_DF, 
+  cols = starts_with("Soil_"), 
+  names_to = "variable", 
+  values_to = "Value"
+)
+SoilNitrogen_DF_long
+
+##### 4.remove the missing data ------------------------------------------------
+SoilNitrogen_DF_long <- SoilNitrogen_DF_long %>%  filter(!is.na(Value))
+
+SoilNitrogen <- SoilNitrogen_DF_long %>%  select(Treatment, Date, Value, variable)
+
+##### 5.tidy up df -------------------------------------------------------------
+
+rm (SoilNitrogen_DF_long, SoilNitrogen_DF)
+
+##### 6.write varibale df -------------------------------------------------------------
+write.csv(SoilNitrogen , "X:/Riskwi$e/Dookie/3_Sims_post_Nov2024/Results/SoilNitrogen_Trial.csv", row.names = FALSE )
+
+
+
+## variable 4 ---------------------------------------------------------------------
+##### 1.select the variable clms ------------------------------------------------
+names(Trial_setup_outputs)
+
+# "Yield (t/ha)"                                                 
+
+Yield_DF <- Trial_setup_outputs %>% 
+  select(Year, Treatment ,"Harvest Date" , 
+         "Yield (t/ha)"    )  
+
+##### 2. rename the columns
+Yield_DF <- Yield_DF %>% rename(
+  Yield = "Yield (t/ha)",
+  Date = "Harvest Date")
+
+
+##### 3.Make the variable clm data long -----------------------------------------
+
+names(Yield_DF)
+# Yield_DF_long <- pivot_longer(
+#   Yield_DF, 
+#   cols = starts_with("Yield_"), 
+#   names_to = "variable", 
+#   values_to = "Value"
+# )
+Yield_DF_long <- Yield_DF %>%  rename(
+  Value = Yield) %>% 
+  mutate(variable = "yield")
+
+##### 4.remove the missing data ------------------------------------------------
+Yield_DF_long <- Yield_DF_long %>%  filter(!is.na(Value))
+Yield_DF_long <- Yield_DF_long %>%  filter(Date > 2017-01-01) # missing dates
+
+Yield <- Yield_DF_long %>%  select(Treatment, Date, Value)
+
+##### 5.tidy up df -------------------------------------------------------------
+
+rm (Yield_DF_long, Yield_DF)
+
+##### 6.write varibale df -------------------------------------------------------------
+write.csv(Yield , "X:/Riskwi$e/Dookie/3_Sims_post_Nov2024/Results/Yield_Trial.csv", row.names = FALSE )
+
+
+
+## variable 5 ---------------------------------------------------------------------
+##### 1.select the variable clms ------------------------------------------------
+names(Trial_setup_outputs)
+
+# "Yield (t/ha)" 
+#"Total N applied at sowing and inseason" 
+
+Yield_Response_N_DF <- Trial_setup_outputs %>% 
+  select(Year, Treatment ,"Harvest Date" , 
+         "Yield (t/ha)" ,
+         "Total N applied at sowing and inseason")  
+
+##### 2. rename the columns
+Yield_Response_N_DF <- Yield_Response_N_DF %>% rename(
+  Yield = "Yield (t/ha)",
+  Date = "Harvest Date",
+  N_applied = "Total N applied at sowing and inseason"
+  )
+
+
+##### 3.Make the variable clm data long -----------------------------------------
+
+names(Yield_Response_N_DF)
+
+
+
+##### 4.remove the missing data ------------------------------------------------
+Yield_Response_N_DF <- Yield_Response_N_DF %>%  filter(!is.na(Yield))
+Yield_Response_N_DF <- Yield_Response_N_DF %>%  filter(!is.na(N_applied))
+#Yield_Response_N_DF <- Yield_Response_N_DF %>%  filter(Date > 2017-01-01) # missing dates
+
+
+
+##### 5.tidy up df -------------------------------------------------------------
+
+
+
+##### 6.write variable df -------------------------------------------------------------
+write.csv(Yield_Response_N_DF , "X:/Riskwi$e/Dookie/3_Sims_post_Nov2024/Results/Yield_Response_N_Trial.csv", row.names = FALSE )
+
+
+
+
+
