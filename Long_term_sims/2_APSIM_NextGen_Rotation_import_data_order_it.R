995572b78c9ac82f57e8d88f4c262be500291434
JackieOuzman <jackie.ouzman@csiro.au>
2025-03-24 14:06:00 +1030

    add the phenology stages

--- Long_term_sims/2_APSIM_NextGen_Rotation_import_data_order_it.R
+++ Long_term_sims/2_APSIM_NextGen_Rotation_import_data_order_it.R
@@ -14,8 +14,26 @@ library(readxl)
 
 # Daily output files -------------------------------------------------------
                                   
-APSIM_NextGen_daily <- read_excel("X:/Riskwi$e/Curyo/3_Sims_post_Nov2024/Curyo_7_Rotation_N_bank.xlsx", 
-                            sheet = "DailyReport")
+APSIM_NextGen_daily <-  read_excel("X:/Riskwi$e/Curyo/4_Long term sims/Curyo_7_Rotation_N_bank_version4.xlsx", 
+                                   col_types = c("text", "numeric", "text", 
+                                                 "numeric", "text", "numeric", "text", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "numeric", 
+                                                 "numeric", "numeric", "text", "text", 
+                                                 "text", "text"))
 str(APSIM_NextGen_daily)
 
 ## Create a new clm called year
@@ -23,21 +41,6 @@ APSIM_NextGen_daily <- APSIM_NextGen_daily %>%
   mutate(Year = year(Date), 
          Source = "NextGen",
          Treatment = Zone,
-         InCropFert = FertNApplied,#check this is the correct clm could be FertAmt 
-         
-         soil_water_start =SwStart ,
-         soil_water_sowing = "NA",
-         soil_water_harvest= "NA",
-         
-         soil_NO3_start = NO3Start,
-         soil_NO3_sowing= "NA", 
-         soil_NO3_harvest ="NA" ,
-         Soil_mineral_N_sowing = "Just use NO3",
-         
-         soil_NH4_start ="not_reported",
-         soil_NH4_sowing ="not_reported",
-         soil_NH4_harvest ="not_reported"
-         
          
          ) 
 
@@ -51,71 +54,46 @@ unique(APSIM_NextGen_daily$Treatment)
 #add in crop and cultivar
 APSIM_NextGen_daily <- APSIM_NextGen_daily %>% 
   mutate(
-Crop = case_when(
-  Year  == 2018 & CurrentState  == "Wheat1"~ "Wheat",
-  Year  == 2019 & CurrentState  == "Canola1"~ "Canola",
-  Year  == 2020 & CurrentState  == "Wheat2"~ "Wheat",
-  Year  == 2021 & CurrentState  == "Barley_1"~ "Barley",
-  Year  == 2022 & CurrentState  == "Wheat3"~ "Wheat",
-  Year  == 2023 & CurrentState  == "Chickpea"~ "Chickpea",
-  Year  == 2024 & CurrentState  == "Barley_2"~ "Barley"  ),
-  
+Crop =CurrentState ,
+
 Cultivar = case_when(
-  Year  == 2018 ~ "scepter",
-  Year  == 2019 ~ "GenericEarly",
-  Year  == 2020 ~ "scepter",
-  Year  == 2021 ~ "Spartacus CL",
-  Year  == 2022 ~ "mace" ,
-  Year  == 2023 ~ "amethyst" ,
-  Year  == 2024 ~ "Spartacus CL" ,
-  ),
-  
+  CurrentState  == "Wheat" ~ "scepter",
+  CurrentState  == "Canola" ~ "GenericEarly",
+  CurrentState  == "Barley" ~ "Spartacus CL",
+  CurrentState  == "Chickpea" ~ "amethyst"), 
+
+Phenology = case_when(
+  CurrentState  == "Wheat" ~ W_Phenology ,
+  CurrentState  == "Canola" ~ C_Phenology,
+  CurrentState  == "Barley" ~ B_Phenology,
+  CurrentState  == "Chickpea" ~ Chick_Phenology), 
+
+
+Protien = case_when(
+  CurrentState  == "Wheat" ~ Protein_W,
+  CurrentState  == "Canola" ~ NA,
+  CurrentState  == "Barley" ~ Protein_B,
+  CurrentState  == "Chickpea" ~ NA), 
+
+ 
 Biomass = case_when(
-  Year  == 2018 ~ AboveGroundW_WtKgha,
-  Year  == 2019 ~ AboveGroundC_WtKgha,
-  Year  == 2020 ~ AboveGroundW_WtKgha,
-  Year  == 2021 ~ AboveGroundB_WtKgha,
-  Year  == 2022 ~ AboveGroundW_WtKgha,
-  Year  == 2023 ~ AboveGroundChick_WtKgha,
-  Year  == 2024 ~ AboveGroundB_WtKgha  ),
+  CurrentState  == "Wheat" ~ AboveGroundW_WtKgha,
+  CurrentState  == "Canola" ~ AboveGroundC_WtKgha,
+  CurrentState  == "Barley" ~ AboveGroundB_WtKgha,
+  CurrentState  == "Chickpea" ~ AboveGroundChick_WtKgha ),
   
+ 
 Zadok = case_when(
-  Year  == 2018 ~ WheatZadok,
-  Year  == 2019 ~ CanolaStage,
-  Year  == 2020 ~ WheatZadok,
-  Year  == 2021 ~ BarleyZadok,
-  Year  == 2022 ~ WheatZadok ,
-  Year  == 2023 ~ ChickpeaStage ,
-  Year  == 2024 ~ BarleyZadok ),
-  
-WaterStress = case_when(
-  Year  == 2018 ~ W_WaterStress,
-  Year  == 2019 ~ C_WaterStress,
-  Year  == 2020 ~ W_WaterStress,
-  Year  == 2021 ~ B_WaterStress,
-  Year  == 2022 ~ W_WaterStress,
-  Year  == 2023 ~ Chick_WaterStress,
-  Year  == 2024 ~ B_WaterStress  ),
-  
-NSTress = case_when(
-  Year  == 2018 ~ W_NSTress,
-  Year  == 2019 ~ C_NSTress,
-  Year  == 2020 ~ W_NSTress,
-  Year  == 2021 ~ B_NSTress,
-  Year  == 2022 ~ W_NSTress ,
-  Year  == 2023 ~ Chick_NSTress ,
-  Year  == 2024 ~ B_NSTress ,
-  ),
-  
+  CurrentState  == "Wheat" ~ WheatZadok,
+  CurrentState  == "Canola" ~ CanolaStage,
+  CurrentState  == "Barley" ~ BarleyZadok,
+  CurrentState  == "Chickpea" ~ ChickpeaStage ),
+ 
 Yield = case_when(
-  Year  == 2018 ~ Yield_W/1000,
-  Year  == 2019 ~ Yield_C/1000,
-  Year  == 2020 ~ Yield_W/1000,
-  Year  == 2021 ~ Yield_B/1000,
-  Year  == 2022 ~ Yield_W/1000,
-  Year  == 2023 ~ Yield_Chick/1000,
-  Year  == 2024 ~ Yield_B/1000,
-  ),
+  CurrentState  == "Wheat" ~ Yield_W/1000,
+  CurrentState  == "Canola" ~ Yield_C/1000,
+  CurrentState  == "Barley" ~ Yield_B/1000,
+  CurrentState  == "Chickpea" ~ Yield_Chick/1000 ),
   
   
 HarvestIndex = (Yield*1000)/Biomass
@@ -143,9 +121,16 @@ APSIM_NextGen_daily <- APSIM_NextGen_daily %>%
     NO3 = `NO3.kgha(1)` + `NO3.kgha(2)` +
       `NO3.kgha(3)` + `NO3.kgha(4)` +
       `NO3.kgha(5)` + `NO3.kgha(6)` + `NO3.kgha(7)`) %>%
-      mutate(Soil_mineral_N_sowing = NO3)
+      mutate(Soil_NO3 = NO3)
       
-     
+APSIM_NextGen_daily <- APSIM_NextGen_daily %>%
+  mutate(
+    NH4 = `NH4.kgha(1)` + `NH4.kgha(2)` +
+      `NH4.kgha(3)` + `NH4.kgha(4)` +
+      `NH4.kgha(5)` + `NH4.kgha(6)` + `NH4.kgha(7)`) %>%
+  mutate(Soil_NH4 = NH4)
+
+
   APSIM_NextGen_daily <- APSIM_NextGen_daily %>%
     mutate(
       Soil_Water =
@@ -162,105 +147,32 @@ max(APSIM_NextGen_daily$Yield)
 
 #Just ordering it
 APSIM_NextGen_daily  <- APSIM_NextGen_daily %>% 
-  select(Date,
-         Year , 
-         Source,
-         Treatment  ,
-         InCropFert  , # check this is the correct clm could be FertAmt 
-         Crop ,
-         Cultivar  ,
+  select( Date, 
+          Year, 
+          Source,           
+          Treatment,            
+          Crop,      
+          Cultivar,
+          Zadok ,
+          Phenology,
+          FertNApplied,
+          #soil_water_start,
+          Soil_Water,
+         # soil_NO3_start,
+          Soil_NO3,
+          Soil_NH4,
+          Biomass, 
+          Yield, 
+          Protien,
+          HarvestIndex
          
-         soil_water_start,
-         #soil_water_sowing,
-         #soil_water_harvest,
-         Soil_Water,
-         
-         soil_NO3_start,
-         #soil_NO3_sowing, #umm is this NO3 + NH4?
-         #soil_NO3_harvest,
-         NO3,
-         
-         # soil_NH4_start,
-         # soil_NH4_sowing,
-         # soil_NH4_harvest,
-         
-         Soil_mineral_N_sowing,
-         
-         Biomass ,
-         Yield  ,
-         Zadok,
-         WaterStress,
-         NSTress,
-         HarvestIndex
-         
-         
-         
-         #InCropRain
   )
 
 
-APSIM_NextGen_daily <- APSIM_NextGen_daily %>% mutate(soil_NO3 = NO3,
-                              zadok_stage = Zadok,
-                              soil_water = Soil_Water)
-
-
-APSIM_NextGen_daily <- APSIM_NextGen_daily %>% select(
-  Date, 
-  Year, 
-  Source,           
-  Treatment,            
-  Crop,      
-  Cultivar,   
-  soil_water, 
-  soil_NO3,  
-  Biomass, 
-  Yield, 
-  zadok_stage ,
-  HarvestIndex,
-  WaterStress,       
-  NSTress,
-  InCropFert)
 
 APSIM_NextGen_daily <- APSIM_NextGen_daily %>% mutate(Biomass = Biomass/1000)
 
 
-# Names of treatments need to match ---------------------------------------
-
-unique(APSIM_NextGen_daily$Treatment)
-# APSIM_NextGen_daily %>% filter(Treatment == "YP_100") %>% 
-#   summarise(max_biomass = max(Biomass),
-#             max_yld = max( Yield))
-# 
-# APSIM_NextGen_daily %>% filter(Treatment == "Maint_150") %>% 
-#   summarise(max_biomass = max(Biomass),
-#             max_yld = max( Yield))
-
-APSIM_NextGen_daily <- APSIM_NextGen_daily %>% mutate(
-  Treatment = case_when(
-    Treatment == "Nil" ~ "Control",
-    Treatment == "National_Av" ~ "National_Av",
-    Treatment == "Maint_100" ~ "Maint_100",
-    Treatment == "Maint_125" ~ "Maint_125",
-    Treatment == "Maint_150" ~ "Maint_150",
-    
-    Treatment == "YP_100" ~ "YP_100",
-    Treatment == "YP_75" ~ "YP_75",
-    Treatment == "YP_50" ~ "YP_50",
-    Treatment == "YP_25" ~ "YP_25",
-    
-    Treatment == "Replacment" ~ "Replacment"#,
-    
-   
-  )
-)
-unique(APSIM_NextGen_daily$Treatment)
-# APSIM_NextGen_daily %>% filter(Treatment == "YP_100") %>% 
-#   summarise(max_biomass = max(Biomass),
-#             max_yld = max( Yield))
-# 
-# APSIM_NextGen_daily %>% filter(Treatment == "Maint_150") %>% 
-#   summarise(max_biomass = max(Biomass),
-#             max_yld = max( Yield))
 
 write.csv(APSIM_NextGen_daily ,
-          "X:/Riskwi$e/Curyo/3_Sims_post_Nov2024/Results/APSIM_NextGen_Daily.csv", row.names = FALSE )
+          "X:/Riskwi$e/Curyo/4_Long term sims/Results/APSIM_NextGen_Daily.csv", row.names = FALSE )
