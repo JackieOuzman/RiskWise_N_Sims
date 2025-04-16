ec9e9c4a6b4086b9c4f2f271f4abf236bf1f53d5
JackieOuzman <jackie.ouzman@csiro.au>
2025-04-16 12:29:45 +0930

    I think we have this sorted now

--- Dry_sowing/Lock/version3/fixed_sowing dates/Lock_step4_PAW.R
+++ Dry_sowing/Lock/version3/fixed_sowing dates/Lock_step4_PAW.R
@@ -61,10 +61,12 @@ str(Anthesis_Flowering)
 Anthesis_Flowering
 
 #### I want to label all the V lines
-
+#harvest is duplicated it comes out of APISM like that why?
+phenology_stages <- phenology_stages %>% 
+  distinct(Wheat.Phenology.CurrentStageName, .keep_all = TRUE)
 plot1 <- Sim_met_may102024 %>% 
   ggplot(aes(x = Clock.Today, PAW_Sum))+
-  geom_vline(data = Anthesis_Flowering, aes(xintercept = Clock.Today), color = "darkgreen", size = 2)+
+  #geom_vline(data = Anthesis_Flowering, aes(xintercept = Clock.Today), color = "darkgreen", size = 2)+
   geom_point()+
   
   theme_classic()+
@@ -75,5 +77,20 @@ plot1 <- Sim_met_may102024 %>%
        subtitle = "Fixed sowing dates 10-May 2024",
        x = "",
        y = "Sum of PAW",
-       caption = "Dashed grey lines indicate phenology stages. Solid grey is frost senstive period (Phenology.Stage between 6.49 - 9.5)")
+       caption = "Dashed grey lines indicate phenology stages.")
 plot1
+
+plot1_labels <- plot1 + geom_text(data = phenology_stages, 
+              mapping = aes(x = Clock.Today, y = c(0.3, 0.5, 0.4, 0.4, 0.4, 0.5, 0.4, 0.4, 0.4,0.4 ,0.4 ), 
+                            label = Wheat.Phenology.CurrentStageName, angle = 90),
+              inherit.aes = FALSE,
+              hjust = 1.0
+              )
+plot1_labels
+
+
+path_saved_files <- file_path_input_data<-file.path("X:","Riskwi$e", "Dry_sowing", "Lock", "Dry_sowing", "Results")
+
+ggsave(plot = plot1_labels,
+       filename = paste0(path_saved_files,"/PAW with phen stages_Lock2024", ".png" ),
+       width = 20, height = 12, units = "cm")
