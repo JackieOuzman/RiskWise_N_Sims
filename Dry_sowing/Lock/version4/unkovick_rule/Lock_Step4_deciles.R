ad64abea7fc6abcf014d77d40e385854b0079993
JackieOuzman <jackie.ouzman@csiro.au>
2025-04-17 15:28:06 +0930

    trying with more sowing dates 'windows'

---
+++ Dry_sowing/Lock/version4/unkovick_rule/Lock_Step4_deciles.R
@@ -0,0 +1,223 @@
+# This file is for assigning rainfall decile years
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(stringr)
+library(lubridate)
+library(readxl)
+
+
+# Download Daily climate files -------------------------------------------------------
+
+climate <- read.table("X:/Riskwi$e/met_files/LOCK_18046.sim", 
+                      skip = 22, header = TRUE, sep ="")
+climate <- climate [-1,]
+### need to make a clm that has all the dates not just day of year and year
+str(climate)
+climate$rain <- as.character(climate$rain)
+climate$rain <- as.double(climate$rain)
+
+#create a date clm # note you need to be aware of the last day of downloaded met data
+
+## Assign dates, site detial and format -------------------------------------------------
+download_date <- read_csv("X:/Riskwi$e/met_files/LOCK_18046.sim",
+                          col_names = FALSE, skip = 7)
+download_date <-download_date[1,1] #just the row with the download date
+download_date <-stringr::str_extract(download_date, "[[:digit:]]+") #just the numbers
+download_date <- as.Date(as.character(download_date),format="%Y%m%d")
+
+#minus one day from download
+download_date <- lubridate::ymd(download_date) - days(1)
+str(download_date)
+
+download_date <-"20250403" # "2025/04/03"
+download_date <- as.Date(as.character(download_date),format="%Y%m%d")
+download_date
+
+
+
+climate <- climate %>% 
+  mutate(date = seq(as.Date("1900/1/1"), download_date, "days"))
+# set date as a date
+climate <- climate %>% 
+  mutate(year = year(date),
+         month =month(date),
+         day_of_month = day(date),
+         month_name = lubridate::month(date, label = TRUE),
+         site = paste0("Lock","_", 018046))
+str(climate)
+
+climate$maxt <- as.numeric(climate$maxt)
+climate$mint <- as.numeric(climate$mint)
+
+## Write out neat climate file ------------------------------------------------
+write.csv(climate ,
+          "X:/Riskwi$e/Dry_sowing/Lock/Dry_sowing/Results/NeatClimate_18046.csv", row.names = FALSE )
+
+
+## Define the GS period and assign season type ---------------------------------
+
+Day_start_GS_rainfall <- 1
+Month_start_GS_rainfall <- 4
+
+
+Day_end_GS_rainfall <- 1
+Month_end_GS_rainfall <- 11
+
+#File start date and end date
+paste("Start date in file is: ",
+      min(climate$date),
+      ". End date in file is: ",
+      max(climate$date))
+
+GS_defined_as <- paste0(
+  Day_start_GS_rainfall, "/", Month_start_GS_rainfall,
+  " to ", Day_end_GS_rainfall, "/", Month_end_GS_rainfall)
+
+str(climate)
+
+# Assign season type to climate data
+
+climate <- climate %>% mutate(
+  start_end_GS_date = case_when(
+    month == Month_start_GS_rainfall & day_of_month == Day_start_GS_rainfall ~ "start_gs",
+    month == Month_end_GS_rainfall & day_of_month == Day_end_GS_rainfall ~ "end_gs",
+    TRUE ~ NA))
+
+# Fill the blanks
+
+climate <- climate %>% fill(start_end_GS_date) %>%
+  mutate(
+    season = case_when(
+      start_end_GS_date == "start_gs" ~ "gs",
+      TRUE ~ "summer"))
+
+climate <- climate %>% select(-start_end_GS_date)
+
+## Dates to use for analysis and filter climate data ---------------------------
+min(climate$date)
+max(climate$date)
+
+start <- "1957-01-01"
+end <- "2024-12-31"
+
+# Filter dataset based on years 
+climate <- climate %>% filter(between(date, as.Date(start), as.Date(end)))
+
+
+## Plot rainfall data grouped into season type ---------------------------------
+
+plot_rainfall_season_type <- climate %>%
+  group_by(year, season) %>%
+  summarise(sum_rain_season = sum(rain, na.rm = TRUE)) %>%
+  
+  
+  ggplot(aes(x = year, y = sum_rain_season, fill = season)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Sum of rain per year",
+    subtitle = paste0(
+    "Station name: ",
+    distinct(climate, site)
+    ),
+    caption = paste0("Years included : ",
+                     year(min(climate$date)) ,
+                     " to ",
+                     year(max(climate$date)),
+                     ". GS defined as: ", 
+                     GS_defined_as),
+    
+    y = "sum of rain")
+plot_rainfall_season_type
+
+
+path_saved_files <- file_path_input_data<-file.path("X:","Riskwi$e", "Dry_sowing", "Lock", "Dry_sowing", "Results")
+
+ggsave(plot = plot_rainfall_season_type,
+       filename = paste0(path_saved_files,"/plot_rainfall_season_type_Lock2023", ".png" ),
+       width = 20, height = 12, units = "cm")
+
+
+## Group by year and season type and report sum of rain -------------------------
+rain_season <- climate %>%
+  group_by(year, season) %>%
+  summarise(sum_rain_season = sum(rain, na.rm = TRUE)) 
+str(rain_season)
+site <- "Lock_18046"
+
+rain_season_wide <- rain_season %>% 
+  pivot_wider(
+    names_from = season,
+    values_from =sum_rain_season 
+  )
+str(rain_season_wide)
+rain_season_wide <- ungroup(rain_season_wide)
+
+rain_season_wide <- rain_season_wide %>% 
+  mutate(site = site,
+         years_included = paste0(year(min(climate$date)) ," to ", year(max(climate$date))),
+         GS = GS_defined_as) 
+str(rain_season_wide)        
+
+
+## new variable gs rain + 0.25 of summer --------------------------------------
+
+rain_season_wide <- rain_season_wide %>% 
+  mutate(gs_plus25_summer = gs+ (0.25*summer))
+
+## Cal percentiles for each year -----------------------------------------------
+
+GS_25_summer_decile_max_rain <- quantile(rain_season_wide$gs_plus25_summer, 
+                               c(.1, .2, .3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0))
+GS_25_summer_deciles_names <- c("decile_1", "decile_2", "decile_3", "decile_4", "decile_5", "decile_6", "decile_7", "decile_8", "decile_9", "decile_10")
+
+GS_25_summer_decile_table <- data.frame(GS_25_summer_decile_max_rain, GS_25_summer_deciles_names, row.names = NULL)
+GS_25_summer_decile_table <- mutate(GS_25_summer_decile_table, GS_25_summer_decile_min_rain = lag(GS_25_summer_decile_max_rain+0.1))
+
+
+#add in the min value to table
+GS_25_summer_decile_table[1,3] <-  min(rain_season_wide$gs_plus25_summer)
+GS_25_summer_decile_table <- GS_25_summer_decile_table %>%  
+  select (GS_25_summer_deciles_names, GS_25_summer_decile_min_rain, GS_25_summer_decile_max_rain)
+
+GS_25_summer_decile_table
+
+GS_25_summer_decile_table <- GS_25_summer_decile_table %>% 
+  mutate( site = site,
+                 years_included = paste0(year(min(climate$date)) ," to ", year(max(climate$date))),
+                 GS = GS_defined_as) 
+
+
+
+GS_25_summer_decile_table_df <- as.data.frame(GS_25_summer_decile_table)
+GS_25_summer_decile_table_df <- GS_25_summer_decile_table_df %>% 
+  mutate(across(where(is.numeric), round, 2))
+GS_25_summer_decile_table_df
+
+
+rain_season_wide <- rain_season_wide %>%
+  mutate(
+    decile = case_when(
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[1,2],1) ,round(GS_25_summer_decile_table_df[1,3],1) ) ~ "decile_1",
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[2,2],1) ,round(GS_25_summer_decile_table_df[2,3],1) ) ~ "decile_2",
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[3,2],1) ,round(GS_25_summer_decile_table_df[3,3],1) ) ~ "decile_3",
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[4,2],1) ,round(GS_25_summer_decile_table_df[4,3],1) ) ~ "decile_4",
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[5,2],1) ,round(GS_25_summer_decile_table_df[5,3],1) ) ~ "decile_5",
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[6,2],1) ,round(GS_25_summer_decile_table_df[6,3],1) ) ~ "decile_6",
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[7,2],1) ,round(GS_25_summer_decile_table_df[7,3],1) ) ~ "decile_7",
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[8,2],1) ,round(GS_25_summer_decile_table_df[8,3],1) ) ~ "decile_8",
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[9,2],1) ,round(GS_25_summer_decile_table_df[9,3],1) ) ~ "decile_9",
+      between(round(gs_plus25_summer, 1), round(GS_25_summer_decile_table_df[10,2],1) ,round(GS_25_summer_decile_table_df[10,3],1) ) ~ "decile_10",
+      
+      TRUE                      ~ "other"
+    )
+  )
+
+path_saved_files <- file_path_input_data<-file.path("X:","Riskwi$e", "Dry_sowing", "Lock", "Dry_sowing", "Results")
+
+
+write_csv(GS_25_summer_decile_table, 
+          file =paste0(path_saved_files, "/GS_25_summer_decile_table_Lock18046", ".csv"))
+
+write_csv(rain_season_wide, 
+          file =paste0(path_saved_files, "/deciles per year_Lock18046", ".csv"))
---
+++ Dry_sowing/Lock/version4/unkovick_rule/Lock_step4_PAW.R
@@ -0,0 +1,97 @@
+## Plant Av water
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(stringr)
+library(lubridate)
+library(readxl)
+
+
+
+
+# Download dry sowing with phenology made in step 3 -------------------------------------------------------
+
+Dry_sowing_Lock_factor_with_met <- read_csv("X:/Riskwi$e/Dry_sowing/Lock/Dry_sowing/Results/Summary_output_phenology_change.csv")
+str(Dry_sowing_Lock_factor_with_met)
+
+unique(Dry_sowing_Lock_factor_with_met$Wheat.Phenology.CurrentStageName)
+
+### pull out the establishment date
+names(Dry_sowing_Lock_factor_with_met)
+Dry_sowing_Lock_factor_with_met <- Dry_sowing_Lock_factor_with_met %>% 
+  select(year,
+         Sowing_date,
+         Clock.Today,
+         Wheat.Phenology.CurrentStageName,
+         Wheat.Phenology.Stage,
+         Yield_Adj_t_ha,
+         PAW_Sum)
+
+
+## Filter just to plot one year and one sowing date - frost year with typical sowing date
+
+Sim_met_may102024 <- Dry_sowing_Lock_factor_with_met %>% 
+  filter(year == 2023) %>% 
+  filter(Sowing_date == "10-may")
+Sim_met_may102024
+
+unique(Sim_met_may102024$Wheat.Phenology.CurrentStageName)
+phenology_stages <- Sim_met_may102024 %>% filter(!is.na(Wheat.Phenology.CurrentStageName)) %>% 
+  select(Clock.Today,Wheat.Phenology.CurrentStageName )
+phenology_stages
+
+str(Sim_met_may102024)
+unique(Sim_met_may102024$frost_Sensitive_period)
+Frost_senstive_period <- Sim_met_may102024 %>% 
+  select(Clock.Today, frost_Sensitive_period ) %>% 
+  filter(frost_Sensitive_period == "frost_sensitive_period" )
+Frost_senstive_period
+
+Frost_most_senstive_period <- Sim_met_may102024 %>% 
+  select(Clock.Today, frost_most_Sensitive_period ) %>% 
+  filter(frost_most_Sensitive_period == "frost_most_sensitive_period" )
+Frost_most_senstive_period
+
+phenology_stages
+
+Anthesis_Flowering <- phenology_stages %>% filter(Wheat.Phenology.CurrentStageName== "Anthesis" )
+
+str(Sim_met_may102024$Clock.Today)
+str(Anthesis_Flowering)
+Anthesis_Flowering
+
+#### I want to label all the V lines
+#harvest is duplicated it comes out of APISM like that why?
+phenology_stages <- phenology_stages %>% 
+  distinct(Wheat.Phenology.CurrentStageName, .keep_all = TRUE)
+plot1 <- Sim_met_may102024 %>% 
+  ggplot(aes(x = Clock.Today, PAW_Sum))+
+  #geom_vline(data = Anthesis_Flowering, aes(xintercept = Clock.Today), color = "darkgreen", size = 2)+
+  geom_point()+
+  
+  theme_classic()+
+  theme(legend.position = "none")+
+  geom_vline(data = phenology_stages, aes(xintercept = Clock.Today), linetype="dashed", color = "grey")+
+  
+  labs(title = "Climate station Lock 18046",
+       subtitle = "Fixed sowing dates 10-May 2023",
+       x = "",
+       y = "Sum of PAW",
+       caption = "Dashed grey lines indicate phenology stages.")
+plot1
+
+plot1_labels <- plot1 + geom_text(data = phenology_stages, 
+              mapping = aes(x = Clock.Today, y = c(0.3, 0.5, 0.4, 0.4, 0.4, 0.5, 0.4, 0.4, 0.4,0.4 ,0.4 ), 
+                            label = Wheat.Phenology.CurrentStageName, angle = 90),
+              inherit.aes = FALSE,
+              hjust = 1.0
+              )
+plot1_labels
+
+
+path_saved_files <- file_path_input_data<-file.path("X:","Riskwi$e", "Dry_sowing", "Lock", "Dry_sowing", "Results")
+
+ggsave(plot = plot1_labels,
+       filename = paste0(path_saved_files,"/PAW with phen stages_Lock2023", ".png" ),
+       width = 20, height = 12, units = "cm")
+
