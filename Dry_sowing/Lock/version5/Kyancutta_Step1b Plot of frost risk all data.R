c015bde039c754c23cacf0aae17fdca844fe7c7b
JackieOuzman <jackie.ouzman@csiro.au>
2025-05-07 10:07:28 +0930

    change GS to frost risk

--- Dry_sowing/Lock/version5/Kyancutta_Step1b Plot of frost risk all data.R
+++ Dry_sowing/Lock/version5/Kyancutta_Step1b Plot of frost risk all data.R
@@ -26,16 +26,16 @@ met_frost <- met_frost %>%
 str(met_frost)
 
 
-### apply a generic GS 
+### apply a generic FR 
 
-## Define the GS period and assign season type ---------------------------------
+## Define the FR period and assign season type ---------------------------------
 
-Day_start_GS_rainfall <- 1
-Month_start_GS_rainfall <- 8 # Aug, it was 4 April
+Day_start_FR_rainfall <- 1
+Month_start_FR_rainfall <- 8 # Aug, it was 4 April
 
 
-Day_end_GS_rainfall <- 1
-Month_end_GS_rainfall <- 10 # Nov, it was 11 Oct
+Day_end_FR_rainfall <- 1
+Month_end_FR_rainfall <- 10 # Nov, it was 11 Oct
 
 #File start date and end date
 paste("Start date in file is: ",
@@ -43,48 +43,48 @@ paste("Start date in file is: ",
       ". End date in file is: ",
       max(met_frost$date))
 
-GS_defined_as <- paste0(
-  Day_start_GS_rainfall, "/", Month_start_GS_rainfall,
-  " to ", Day_end_GS_rainfall, "/", Month_end_GS_rainfall)
+FR_defined_as <- paste0(
+  Day_start_FR_rainfall, "/", Month_start_FR_rainfall,
+  " to ", Day_end_FR_rainfall, "/", Month_end_FR_rainfall)
 
 str(met_frost)
 
 # Assign season type to climate data
 
 met_frost <- met_frost %>% mutate(
-  start_end_GS_date = case_when(
-    month == Month_start_GS_rainfall & day_of_month == Day_start_GS_rainfall ~ "start_gs",
-    month == Month_end_GS_rainfall & day_of_month == Day_end_GS_rainfall ~ "end_gs",
+  start_end_FR_date = case_when(
+    month == Month_start_FR_rainfall & day_of_month == Day_start_FR_rainfall ~ "start_FR",
+    month == Month_end_FR_rainfall & day_of_month == Day_end_FR_rainfall ~ "end_FR",
     TRUE ~ NA))
 
 # Fill the blanks
 
-met_frost <- met_frost %>% fill(start_end_GS_date) %>%
+met_frost <- met_frost %>% fill(start_end_FR_date) %>%
   mutate(
     season = case_when(
-      start_end_GS_date == "start_gs" ~ "gs",
+      start_end_FR_date == "start_FR" ~ "FR",
       TRUE ~ "summer"))
 
-met_frost <- met_frost %>% select(-start_end_GS_date)
+met_frost <- met_frost %>% select(-start_end_FR_date)
 
 met_frost <- met_frost %>%
-  mutate(GS_definition = GS_defined_as)
+  mutate(FR_definition = FR_defined_as)
 
 
-met_frost_GS <- met_frost %>% filter(season == "gs")
+met_frost_FR <- met_frost %>% filter(season == "FR")
 
 ## frost days
-## Summaries data how many frost days in GS by year----
-names(met_frost_GS)
-str(met_frost_GS)
+## Summaries data how many frost days in FR by year----
+names(met_frost_FR)
+str(met_frost_FR)
 
-days_GS <- met_frost_GS %>% 
+days_FR <- met_frost_FR %>% 
   group_by(year ) %>% 
   count(season)
-days_GS <- days_GS %>%  rename(days_GS = n)
-days_GS
+days_FR <- days_FR %>%  rename(days_FR = n)
+days_FR
 
-frost_event_count_year <- met_frost_GS %>% 
+frost_event_count_year <- met_frost_FR %>% 
   group_by(year, frost_event) %>% 
   count(frost_event)
 
@@ -96,19 +96,19 @@ frost_event_count_year
 
 
 
-summary_frost_details <- left_join(frost_event_count_year, days_GS)
+summary_frost_details <- left_join(frost_event_count_year, days_FR)
 summary_frost_details <- summary_frost_details %>% mutate(
-  frost_event_percent = (frost_event_count/ days_GS)*100)
-summary_frost_details_all_yrs <- left_join(days_GS, summary_frost_details)
+  frost_event_percent = (frost_event_count/ days_FR)*100)
+summary_frost_details_all_yrs <- left_join(days_FR, summary_frost_details)
 
 summary_frost_details_all_yrs <- summary_frost_details_all_yrs %>% 
-  select(year, days_GS, frost_event_count, frost_event_percent) %>% 
-  mutate(GS_definition = GS_defined_as) %>% 
+  select(year, days_FR, frost_event_count, frost_event_percent) %>% 
+  mutate(FR_definition = FR_defined_as) %>% 
   mutate(frost_event_count = ifelse(is.na(frost_event_count), 0, frost_event_count),
          frost_event_percent = ifelse(is.na(frost_event_percent), 0, frost_event_percent))
 
-unique(days_GS$days_GS)
-GS_defined_as
+unique(days_FR$days_FR)
+FR_defined_as
 
 
 plot1 <- summary_frost_details_all_yrs %>% 
@@ -119,7 +119,7 @@ plot1 <- summary_frost_details_all_yrs %>%
         axis.text.x = element_text(angle = 90, hjust = 1))+
   scale_y_continuous(limits = c(0, 11))+
   labs(title = "Frost Risk at Kyancutta climate station number 18170",
-       subtitle = paste0("Standard GS of ", unique(days_GS$days_GS)," days, defined as ", GS_defined_as),
+       subtitle = paste0("Period of frost risk ", unique(days_FR$days_FR)," days, defined as ", FR_defined_as),
        x = "years",
        y = "Count of days classified as frost (< 1)"
   )
@@ -134,7 +134,7 @@ plot1_2015_2024 <- summary_frost_details_all_yrs %>%
         axis.text.x = element_text(angle = 90, hjust = 1))+
   scale_y_continuous(limits = c(0, 11))+
   labs(title = "Frost Risk at Kyancutta climate station number 18170",
-       subtitle = paste0("Standard GS of ", unique(days_GS$days_GS)," days, defined as ", GS_defined_as),
+       subtitle = paste0("Period of frost risk ", unique(days_FR$days_FR)," days, defined as ", FR_defined_as),
        x = "years",
        y = "Count of days classified as frost (< 1)"
   )
@@ -148,7 +148,7 @@ plot2_perc <- summary_frost_details_all_yrs %>%
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 90, hjust = 1))+
   labs(title = "Frost Risk at Kyancutta climate station number 18170",
-       subtitle = paste0("Standard GS of ", unique(days_GS$days_GS)," days, defined as ", GS_defined_as),
+       subtitle = paste0("Standard FR of ", unique(days_FR$days_FR)," days, defined as ", FR_defined_as),
        x = "years",
        y = "Percentage of days classified as frost (< 1)"
   )
@@ -162,7 +162,7 @@ plot2_2015_2024perc <- summary_frost_details_all_yrs %>%
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 90, hjust = 1))+
   labs(title = "Frost Risk at Kyancutta climate station number 18170",
-       subtitle = "Standard GS of 214 day. 1/4 to 1/11",
+       subtitle = "Period of frost risk of 214 day. 1/4 to 1/11",
        x = "years",
        y = "Percentage of days classified as frost (< 1)"
   )
@@ -174,27 +174,27 @@ plot2_2015_2024perc
 path_saved_files <- file_path_input_data<-file.path("X:","Riskwi$e", "Dry_sowing", "Lock", "Dry_sowing","version5" , "Results")
 path_saved_files
 ggsave(plot = plot1,
-       filename = paste0(path_saved_files,"/Climate_Frost_days_Kyancutta_std_GS", ".png" ),
+       filename = paste0(path_saved_files,"/Climate_Frost_days_Kyancutta_std_FR", ".png" ),
        width = 20, height = 12, units = "cm")
 
 
 ggsave(plot = plot1_2015_2024,
-       filename = paste0(path_saved_files,"/Climate_Frost_days_Kyancutta_std_GS_2014_2024", ".png" ),
+       filename = paste0(path_saved_files,"/Climate_Frost_days_Kyancutta_std_FR_2014_2024", ".png" ),
        width = 20, height = 12, units = "cm")
 
 
 
 ggsave(plot = plot2_perc,
-       filename = paste0(path_saved_files,"/Climate_Frost_days_Kyancutta_std_GS_Prec", ".png" ),
+       filename = paste0(path_saved_files,"/Climate_Frost_days_Kyancutta_std_FR_Prec", ".png" ),
        width = 20, height = 12, units = "cm")
 
 
 ggsave(plot = plot2_2015_2024perc,
-       filename = paste0(path_saved_files,"/Climate_Frost_days_Kyancutta_std_GS_2014_2024_Prec", ".png" ),
+       filename = paste0(path_saved_files,"/Climate_Frost_days_Kyancutta_std_FR_2014_2024_Prec", ".png" ),
        width = 20, height = 12, units = "cm")
 
 
 
 write_csv(summary_frost_details_all_yrs,
-          file = paste0(path_saved_files,"/summary_frost_details_Kyancutta_Std_GS", ".csv"))
+          file = paste0(path_saved_files,"/summary_frost_details_Kyancutta_Std_FR", ".csv"))
 
